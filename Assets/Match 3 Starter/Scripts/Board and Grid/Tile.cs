433b31fe1d8911b6b1eaff8453160a6dded0d327
Ujjwal Sharma <usharma1998@gmail.com>
2018-05-03 03:01:06 +0530

    Add tile swapping

--- Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
+++ Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
@@ -2,23 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Tile : MonoBehaviour {
+public class Tile: MonoBehaviour {
 	private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
 	private static Tile previousSelected = null;
 
 	private SpriteRenderer render;
 	private bool isSelected = false;
 
-	private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+	private Vector2[] adjacentDirections = new Vector2[] {
+		Vector2.up,
+		Vector2.down,
+		Vector2.left,
+		Vector2.right
+	};
 
 	void Awake() {
-		render = GetComponent<SpriteRenderer>();
-    }
+		render = GetComponent < SpriteRenderer > ();
+	}
 
 	private void Select() {
 		isSelected = true;
 		render.color = selectedColor;
-		previousSelected = gameObject.GetComponent<Tile>();
+		previousSelected = gameObject.GetComponent < Tile > ();
 		SFXManager.instance.PlaySFX(Clip.Select);
 	}
 
@@ -26,6 +31,55 @@ public class Tile : MonoBehaviour {
 		isSelected = false;
 		render.color = Color.white;
 		previousSelected = null;
+	}
+
+	void OnMouseDown() {
+		if (render.sprite == null || BoardManager.instance.IsShifting) {
+			return;
+		}
+
+		if (isSelected) {
+			Deselect();
+		} else {
+			if (previousSelected == null) {
+				Select();
+			} else {
+				if (GetAllAdjacentTiles().Contains(previousSelected.gameObject)) {
+					SwapSprite(previousSelected.render);
+					previousSelected.Deselect();
+				} else {
+					previousSelected.GetComponent < Tile > ().Deselect();
+					Select();
+				}
+			}
+		}
+	}
+
+	public void SwapSprite(SpriteRenderer render2) { // 1
+		if (render.sprite == render2.sprite) { // 2
+			return;
+		}
+
+		Sprite tempSprite = render2.sprite; // 3
+		render2.sprite = render.sprite; // 4
+		render.sprite = tempSprite; // 5
+		SFXManager.instance.PlaySFX(Clip.Swap); // 6
+	}
+
+	private GameObject GetAdjacent(Vector2 castDir) {
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
+		if (hit.collider != null) {
+			return hit.collider.gameObject;
+		}
+		return null;
+	}
+
+	private List < GameObject > GetAllAdjacentTiles() {
+		List < GameObject > adjacentTiles = new List < GameObject > ();
+		for (int i = 0; i < adjacentDirections.Length; i++) {
+			adjacentTiles.Add(GetAdjacent(adjacentDirections[i]));
+		}
+		return adjacentTiles;
 	}
 
 }
