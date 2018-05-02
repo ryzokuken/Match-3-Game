4706d04122aec11e94005aaa4e9ffaa84a67ff4e
Ujjwal Sharma <usharma1998@gmail.com>
2018-05-03 03:46:39 +0530

    Display game over

--- Assets/Match 3 Starter/Scripts/Managers/GUIManager.cs
+++ Assets/Match 3 Starter/Scripts/Managers/GUIManager.cs
@@ -33,6 +33,10 @@ public class GUIManager : MonoBehaviour {
 
 		set {
 			moveCounter = value;
+			if (moveCounter <= 0) {
+				moveCounter = 0;
+				StartCoroutine(WaitForShifting());
+			}
 			moveCounterTxt.text = moveCounter.ToString();
 		}
 	}
@@ -57,6 +61,14 @@ public class GUIManager : MonoBehaviour {
 		}
 
 		yourScoreTxt.text = score.ToString();
+	}
+
+	private IEnumerator WaitForShifting() {
+		yield
+		return new WaitUntil(() => !BoardManager.instance.IsShifting);
+		yield
+		return new WaitForSeconds(.25f);
+		GameOver();
 	}
 
 }
