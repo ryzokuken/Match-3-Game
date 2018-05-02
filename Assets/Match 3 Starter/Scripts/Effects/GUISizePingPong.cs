84f9a895495aad1a2f999cf7e39a29127dc41d79
Ujjwal Sharma <usharma1998@gmail.com>
2018-05-02 11:01:29 +0530

    Remove copyright text

--- Assets/Match 3 Starter/Scripts/Effects/GUISizePingPong.cs
+++ Assets/Match 3 Starter/Scripts/Effects/GUISizePingPong.cs
@@ -1,26 +1,4 @@
-﻿/*
- * Copyright (c) 2017 Razeware LLC
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(RectTransform))]
@@ -39,7 +17,7 @@ public class GUISizePingPong : MonoBehaviour {
 	IEnumerator PingPongSize() {
 		Vector3 startingScale = rectTransform.localScale;
 		Vector3 currentScale = startingScale;
-		
+
 		// How much to change the scale each iteration
 		Vector3 changeScale = new Vector3(.01f, .01f, .01f);
 		bool shrink = true;
