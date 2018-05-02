6bea187f074125b6b8b851e005bc46901ea26df2
Ujjwal Sharma <usharma1998@gmail.com>
2018-05-02 17:06:08 +0530

    Improve BoardManager

--- Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
+++ Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
@@ -16,21 +16,42 @@ public class BoardManager : MonoBehaviour {
 		instance = GetComponent<BoardManager>();
 
 		Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
-        CreateBoard(offset.x, offset.y);
-    }
+		CreateBoard(offset.x, offset.y);
+	}
 
 	private void CreateBoard (float xOffset, float yOffset) {
 		tiles = new GameObject[xSize, ySize];
 
-        float startX = transform.position.x;
+		float startX = transform.position.x;
 		float startY = transform.position.y;
+
+		Sprite[] previousLeft = new Sprite[ySize];
+		Sprite previousBelow = null;
 
 		for (int x = 0; x < xSize; x++) {
 			for (int y = 0; y < ySize; y++) {
 				GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
 				tiles[x, y] = newTile;
+
+				// Parent all the tiles to your BoardManager to keep your Hierarchy clean in the editor
+				newTile.transform.parent = transform;
+
+				// Create a list of possible characters for this sprite
+				List<Sprite> possibleCharacters = new List<Sprite>();
+				// Add all characters to the list
+				possibleCharacters.AddRange(characters);
+
+				// Remove the characters that are on the left and below the current sprite from the list of possible characters
+				possibleCharacters.Remove(previousLeft[y]);
+				possibleCharacters.Remove(previousBelow);
+
+				Sprite newSprite = possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+				newTile.GetComponent<SpriteRenderer>().sprite = newSprite;
+
+				previousLeft[y] = newSprite;
+				previousBelow = newSprite;
 			}
-        }
-    }
+		}
+	}
 
 }
